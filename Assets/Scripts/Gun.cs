b16bec256c04fc6c59b1270aaec8a16f6035fe01
Mads Pedersen <mads.msc@gmail.com>
2020-09-05 13:40:51 +0200

    first big update

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +10,7 @@ public class Gun : MonoBehaviour {
     public GameObject textGO;
     public GameObject explosion;
     public GameObject flashGO;
+    public GameObject muzzle;
     public int fireRate = 0; // how many 1/70s between shots
     
     private int fireCD = 0;
@@ -17,35 +20,46 @@ public class Gun : MonoBehaviour {
 
     void Start () {
     	audioSource = gameObject.AddComponent<AudioSource>();
-	audioSource.clip = clip;
-	flash = flashGO.GetComponent<ParticleSystem>();
+	    audioSource.clip = clip;
+	    flash = flashGO.GetComponent<ParticleSystem>();
         text = textGO.GetComponent<TextMesh>();
-	text.text = "0";
-	Disable();
+	    text.text = "0";
+
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.005f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.startColor = Color.green;
+        lineRenderer.endColor = Color.green;
+
+        Disable();
     }
 
-    public void Disable(){
+    public void Disable() {
         textGO.SetActive(false);
-	flashGO.SetActive(false);
+	    flashGO.SetActive(false);
     }
 
-    public void Enable(){
+    public void Enable() {
         textGO.SetActive(true);
-	flashGO.SetActive(true);
+        flashGO.SetActive(true);
     }
 
     void Update() {
-	if(FullAuto() && OnCD()){
-	    fireCD++;
-	}
+	    if(FullAuto() && OnCD()){
+	        fireCD++;
+	    }
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, muzzle.transform.position);
+        lineRenderer.SetPosition(1, muzzle.transform.position + muzzle.transform.forward * 5);
     }
 
     public bool OnCD() {
-	return fireCD < fireRate;
+	    return fireCD < fireRate;
     }
 
     public bool FullAuto() {
-	return fireRate > 0;
+	    return fireRate > 0;
     }
 
     private int shots = 0;
@@ -53,16 +67,17 @@ public class Gun : MonoBehaviour {
     public void Fire() {
     	text.text = (++shots).ToString();
         fireCD = 0;	
-	flash.Play();
-	audioSource.Play();
+	    flash.Play();
+	    audioSource.Play();
         RaycastHit hit;
-	if(Physics.Raycast(transform.position, transform.forward, out hit)){
-	    GameObject target = hit.collider.gameObject;
-	    if(target.CompareTag("Cube")){
-	        Destroy(target);
-		GameObject ex1 = Instantiate(explosion);
-		ex1.transform.position = target.transform.position;
+
+        if (Physics.Raycast(muzzle.transform.position, muzzle.transform.forward, out hit)){
+	        GameObject target = hit.collider.gameObject;
+	        if(target.CompareTag("Cube")){
+	            Destroy(target);
+		        GameObject ex1 = Instantiate(explosion);
+		        ex1.transform.position = target.transform.position;
+	        }
 	    }
-	}
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public float startHealth = 5;
+    public float health;
+    public Camera cam;
+    private Slider slider;
+    public bool initialized = false;
+
+    void Start()
+    {
+        health = startHealth;
+        slider = GetComponentInChildren<Slider>();
+        slider.value = 1f;
+        initialized = true;
+    }
+
+    private void Update()
+    {
+        slider.transform.LookAt(cam.transform);
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        slider.value = health / startHealth;
+        //Debug.Log(gameObject.name + " taking " + damage + " damage. " + health + " hp left.");
+        if (health <= 0)
+            die();
+    }
+
+    public void die() {
+        gameObject.SetActive(false);
+    }
+}
